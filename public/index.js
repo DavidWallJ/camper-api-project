64b74a5cef6d96407b1ca4b01fb03fd6472ed867
DavidWallJ <djkindread@gmail.com>
2017-05-02 10:15:20 +0800

    added a loader to the results section

--- public/index.js
+++ public/index.js
@@ -1,25 +1,35 @@
 /**
- * Created by david on 5/1/17.
- */
+* Created by david on 5/1/17.
+*/
 
 $(document).ready(function () {
     var date;
 
     $("form").submit(function (event) {
-        dateObject = ( $(this).serializeArray() );
-        event.preventDefault();
-        date = dateObject[0].value;
+    dateObject = ( $(this).serializeArray() );
+    event.preventDefault();
+    date = dateObject[0].value;
 
 
-        var url = "https://dateconverter.herokuapp.com/dateValues/" + date;
+    var url = "https://dateconverter.herokuapp.com/dateValues/" + date;
 
-
-        $.get(url, function (data) {
+    $.ajax({
+        type: "GET",
+        url: url,
+        beforeSend: function() {
+            $( ".loader" ).css("visibility", "visible");
+        },
+        complete: function() {
+            $( ".loader" ).css("visibility", "hidden");
+        },
+        success: function(data) {
             $( ".results" ).html(
                 "<p class='text-center'><span>Natural Date: </span>" + data.natural + "</p>" +
                 "<p class='text-center'><span>Seconds since January 1, 1970 (UTC): </span>" + data.unix + "</p>"
             );
-        });
+        }
+
+    });
 
     });
 });
